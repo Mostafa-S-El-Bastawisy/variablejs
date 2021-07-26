778a4fe116a5cf164c91989883fed093850c574d
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-07-26 12:53:35 +0200

    Add file

---
+++ main.js
@@ -0,0 +1,8 @@
+let markup = `
+    <div class="card">
+      <h3>Hello Elzero</h3>
+      <p>Elzero Web School</p>
+      <span>25/10</span>
+    </div>
+    `;
+    document.write(markup.repeat(4));
